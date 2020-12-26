8f85caa925283902ed88d1c3c8e3c0c924b79975
jtimonen <juho.timonen@iki.fi>
2020-12-26 21:48:00 +0200

    add c-shape distribution

--- server.R
+++ server.R
@@ -5,10 +5,19 @@ sample_gaussian <- function(n, m, s) {
   return(x)
 }
 
+sample_c <- function(n, sigma, R) {
+  t <- seq(0, pi, length.out = n)
+  x1 <- R*cos(t) + stats::rnorm(n, 0, sigma)
+  x2 <- R*sin(t) + stats::rnorm(n, 0, sigma)
+  x <- cbind(x1, x2)
+  return(x)
+}
+
 server <- function(input, output) {
   pch <- 16
   S <- 6
   output$plot1 <- renderPlot({
+    set.seed(input$seed)
     m1 <- c(input$slider_m11, input$slider_m12)
     m2 <- c(0, 0)
     s1 <- c(input$slider_s11, input$slider_s12)
@@ -16,9 +25,15 @@ server <- function(input, output) {
     n1 <- input$slider_n1
     n2 <- input$slider_n2
     var <- input$slider_var
+    if (input$dist == "Gaussian") {
+      y <- sample_gaussian(n2, m2, s2)
+    } else {
+      y <- sample_c(n2, 0.2, 2.0)
+    }
     x <- sample_gaussian(n1, m1, s1)
-    y <- sample_gaussian(n2, m2, s2)
-    mmd <- MMD(x, y, var = var*diag(2))
+    mmd <- MMD(x, y, var = var*diag(2), bias = input$bias)
+    lab <- if (input$bias) "MMD = " else "MMD_u = "
+    mmd <- paste0(lab, round(mmd, 5))
     plot(x[,1], x[,2], pch = pch, col = "firebrick", main = mmd,
          xlim = c(-S,S), ylim = c(-S,S), 
          xlab = expression(x[1]), ylab = expression(x[2]))
--- ui.R
+++ ui.R
@@ -16,7 +16,8 @@ body <- dashboardBody(
               box(plotOutput("plot1", height = 550)),
               
               box(
-                title = "Controls",
+                width = 3,
+                title = "Distribution p",
                 
                 sliderInput("slider_m11", "Mean 1:", -3.0, 3.0, 0.0, step = h),
                 sliderInput("slider_m12", "Mean 2:", -3.0, 3.0, 0.0, step = h),
@@ -24,9 +25,24 @@ body <- dashboardBody(
                 sliderInput("slider_s11", "Std 1:", 0.0, 3.0, 1.0, step = h),
                 sliderInput("slider_s12", "Std 2:", 0.0, 3.0, 1.0, step = h),
                 
-                sliderInput("slider_n1", "n_1:", 1, 500, 200),
-                sliderInput("slider_n2", "n_2:", 1, 500, 200),
-                sliderInput("slider_var", "MMD var:", 0.0, 10.0, 1.0, step = h)
+                sliderInput("slider_n1", "n_1:", 1, 1000, 200),
+              ),
+              box(
+                width = 3, 
+                title = "Distribution q",
+                sliderInput("slider_n2", "n_2:", 1, 1000, 200),
+                selectInput(
+                  "dist", "Distribution", list("Gaussian", "C-shape")
+                )
+              ),
+              box(
+                width = 3,
+                title = "Controls",
+                sliderInput("slider_var", "MMD var:", 0.0, 10.0, 1.0, step = h),
+                radioButtons("bias", "Biased estimator:",
+                             choiceNames = list("TRUE", "FALSE"),
+                             choiceValues = list(TRUE, FALSE)),
+                numericInput("seed", "RNG seed", 123, step = 1)
               )
             )
     ),
